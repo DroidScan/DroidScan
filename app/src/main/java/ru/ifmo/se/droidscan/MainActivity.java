934ddf80618b790654b53b4beb959713b888c94a
ekaterina <kartasheva.ekaterina.elets@gmail.com>
2020-02-06 15:54:05 +0300

    using camera app

--- app/src/main/java/ru/ifmo/se/droidscan/MainActivity.java
+++ app/src/main/java/ru/ifmo/se/droidscan/MainActivity.java
@@ -1,14 +1,70 @@
-package ru.ifmo.se.droidscan;
+package com.example.droidscan;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.ImageView;
+
+import java.io.File;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final int CAMERA_REQUEST = 0;
+    private ImageView imageView;
+    private Uri outputFileUri;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        imageView = findViewById(R.id.imageView);
+
     }
+
+    public void onClick(View v) {
+        getThumbnailPicture();
+        //   saveFullPicture();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == CAMERA_REQUEST && resultCode == RESULT_OK) {
+            // Проверяем, содержит ли результат маленькую картинку
+            if (data != null) {
+                if (data.hasExtra("data")) {
+                    Bitmap thumbnailBitmap = data.getParcelableExtra("data");
+                    imageView.setImageBitmap(thumbnailBitmap);
+                }
+            } else {
+                // Какие-то действия с полноценным изображением,
+                // сохраненным по адресу outputFileUri
+                imageView.setImageURI(outputFileUri);
+            }
+        }
+    }
+
+    public void getThumbnailPicture() {
+        Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivityForResult(cameraIntent, CAMERA_REQUEST);
+    }
+
+    public void saveFullPicture() {
+        Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        File file = new File(Environment.getExternalStorageDirectory(),
+                "test.jpg");
+        outputFileUri = Uri.fromFile(file);
+        cameraIntent.putExtra(MediaStore.EXTRA_OUTPUT, outputFileUri);
+        startActivityForResult(cameraIntent, CAMERA_REQUEST);
+    }
+
+
 }
